21939eb7e34c6003cd0412b07f5dcb83f9c0d483
Dinesh Thogulua <saravanadinesh@gmail.com>
2020-08-11 21:30:19 +0530

    Coding complete. Partially typo corrected and partially working

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def spherical3d(x, y, z, angle_unit = 'radians'):
     theta = np.arctan2(y, x)
     phi = np.arctan2((x**2+y**2)**0.5, z)
     if angle_unit == 'degrees':
-        theta = (theta * 180 / pi)
-        phi = (theta * 180 / pi)
+        theta = (theta * 180 / np.pi)
+        phi = (phi * 180 / np.pi)
     return r, theta, phi
 
 # Define a rectangular surface
@@ -80,21 +80,21 @@ def spherical3d(x, y, z, angle_unit = 'radians'):
 #    point
 
 # Parameters of the rectangular LED mat light source
-led_mat_rows = 10 # no unit. Must be an integer
-led_mat_cols = 10 # no unit. Must be an integer
+led_mat_rows = 2  # no unit. Must be an integer
+led_mat_cols = 2  # no unit. Must be an integer
 num_leds = led_mat_rows * led_mat_cols # number of LEDs; one LED at every intersection of a column and a row
-led_mat_len = 0.3 # meters
+led_mat_len = 1  # meters
 led_mat_wid = led_mat_len * led_mat_rows / led_mat_cols # meters
 
 # Individual LED parameters 
 led_view_angle = 110 # degrees
-led_max_output = 100 # lumens per LED
+led_max_output = 1000 # lux at 1m
 
 # Position parameters
-r_1, theta_1, phi_1 = 1, 45, 0 # One of the vectors lying on the plane, when the plane's center is 
+r_1, theta_1, phi_1 = 1, 45, 90 # One of the vectors lying on the plane, when the plane's center is
                                # mapped to the origin
-r_2, theta_2, phi_2 = 1, 45, 90 # The second vector
-mat_dist = np.array([[1, 1, 0]]).T # meters. How far is the center of the mat away from the origin
+r_2, theta_2, phi_2 = 1, 45, 0 # The second vector
+mat_dist = np.array([[4, -4, 0]]).T # meters. Location of the center of the LED mat in 3d space
 
 # Generate LED mat surface coordinates
 x1, y1, z1 = cart3d(r_1, theta_1, phi_1, angle_unit = 'degrees') # Convert to cartesian coordiantes
@@ -114,7 +114,7 @@ U, S, Vh = la.svd(A) # Since A is 2x3 matrix, the last column of V would represe
 plane_params_origin = Vh[2,:] # i.e, plane parameters [a,b,c] = last row of Vh, where the plane equation is
                               # ax+by+cz = 0. Keyword '_origin' is used to indeicate that this is the version
                               # of the plane after translating its mid point to the origin
-plane_params = plane_params_origin + mat_dist # Translate the plane to its actual intended locaiton
+plane_params = plane_params_origin + mat_dist # Translate the plane to its actual intended location
 
 led_plane = np.empty([3, led_mat_rows * led_mat_cols]) # A collection of all the points that comprise the LED 
                                                        # plane
@@ -139,14 +139,13 @@ for row in range(0, led_mat_rows):
 led_x = led_x + mat_dist[0,0]
 led_y = led_y + mat_dist[1,0]
 led_z = led_z + mat_dist[2,0]
-
-
+led_plane = led_plane + mat_dist
 
 
 # Define the surface of the sphere
 s_centre = np.array([[0,0,0]]).T # Sphere center 
-s_radius = 0.3 # meters. Radius of the sphere
-pixel_step = 20 # degrees. The granularity of theta, phi variations (lat, lon)
+s_radius = 1 # meters. Radius of the sphere
+pixel_step = 45 # degrees. The granularity of theta, phi variations (lat, lon)
 pix_thetas = np.arange(start = 0, stop = 360, step = pixel_step)
 pix_phis = np.arange(start = 0, stop = 360, step = pixel_step)
 pix_thetas, pix_phis = np.meshgrid(pix_thetas, pix_phis)
@@ -157,60 +156,73 @@ pix_z = pix_z + s_centre[2,0]
 num_pixels = pix_x.size
 num_pix_rows, num_pix_cols = pix_x.shape
 pixels = np.empty([3,num_pixels])
-pixels = np.concatenate((pix_x.reshape([1,num_pixes]), pix_y.reshape([1,num_pixels]), pix_z.reshape([1,num_pixels])), axis = 0) # Make an array of all pixel coordinates
+pixels = np.concatenate((pix_x.reshape([1,num_pixels]), pix_y.reshape([1,num_pixels]), pix_z.reshape([1,num_pixels])), axis = 0) # Make an array of all pixel coordinates
+
+# Define camera parameters
+cam_location = np.array([[2,0,0]]).T
 
 # Compute the impact of every LED on pixels, one pixed at a time
 pix_led_impact_mat = np.zeros([num_pixels, num_leds])
 reflectivity_sigma = 45 # degrees. Surface reflectivity is modeled as a 2D gaussian with theta and phi as the variables   
 for pixel in range(0, num_pixels):
     for led in range(0, led_mat_rows * led_mat_cols):
+        if la.norm(led_plane[:,led]) <= s_radius: # Skip LEDs inside the sphere
+            print('WARNING: LED inside sphere')
+            continue
         # Find if the pixel can get any light at all from the LED in question
-        pixled_norm = np.dot(pixels[:,pixel], led_plane[:,led])
-        pixpix_norm = np.dot(pixels[:,pixel], pixels[:,pixel])
-        if (pixled_norm > pixpix_norm): # This means the light from the LED in question cannot reach this pixel, 
+        pixled_ip = np.dot(pixels[:,pixel], led_plane[:,led])
+        pixpix_ip = np.dot(pixels[:,pixel], pixels[:,pixel])
+        if pixled_ip > pixpix_ip: # This means the light from the LED in question cannot reach this pixel,
                                         # because the pixel is on the "back side" of the sphere w.r.to this LED
+            #print(str(pixels[:,pixel]), ' not reachable to LED ', str(led_plane[:,led]))
             continue
         # Find the rotation matrix to be used to find new coordinates for vectors after rotating the axes to have z-axis 
         # align with the vector representing the pixel point we are looking at in this iteration 
         rot_theta = -pix_thetas[int(pixel/num_pix_cols), pixel%num_pix_cols] # Negative sign because we need to rotate all vectors
         rot_phi = -pix_phis[int(pixel/num_pix_cols), pixel%num_pix_cols]  # clockwise by this theta and phi
-        rotation_mat1 = np.array([cos(rot_theta), -sin(rot_theta), 0],[sin(rot_theta), cos(rot_theta), 0],[0, 0, 1]) # Rotation around z axis
-        rotation_mat2 = np.array([cos(rot_phi), 0, sin(rot_phi)], [0, 1, 0], [-sin(rot_phi), 0, cos(rot_phi)]]) # Rotation around y axis
+        rotation_mat1 = np.array([[np.cos(rot_theta), -np.sin(rot_theta), 0],[np.sin(rot_theta), np.cos(rot_theta), 0],[0, 0, 1]]) # Rotation around z axis
+        rotation_mat2 = np.array([[np.cos(rot_phi), 0, np.sin(rot_phi)], [0, 1, 0], [-np.sin(rot_phi), 0, np.cos(rot_phi)]]) # Rotation around y axis
         # Translate and rotate the incident light line and cam-pixel line
-        tranlated_led_loc  = led_plane[:,led:led+1] - pixels[:,pixel:pixel+1] # First we translate the origin to convert the incident light line to a vector
+        translated_led_loc  = led_plane[:,led:led+1] - pixels[:,pixel:pixel+1] # First we translate the origin to convert the incident light line to a vector
         translated_cam_loc = cam_location - pixels[:,pixel:pixel+1]
         rotated_inc_vector = rotation_mat2 @ rotation_mat1 @ translated_led_loc # Then we rotate the incident light vector
         rotated_cam_vector = rotation_mat2 @ rotation_mat1 @ translated_cam_loc
         # Now find the theta and phi of the incident light and camera-pix line on this bases system            
-        inc_r, inc_theta, inc_phi = spherical3d(rotated_inc_vector[0,0], rotated_inc_vector[0,1], rotated_inc_vector[0,3], angle_unit = 'degrees')
-        cam_r, cam_theta, cam_phi = spherical3d(rotated_cam_vector[0,0], rotated_cam_vector[0,1], rotated_cam_vector[0,3], angle_unit = 'degrees')
+        inc_r, inc_theta, inc_phi = spherical3d(rotated_inc_vector[0,0], rotated_inc_vector[1,0], rotated_inc_vector[2,0], angle_unit = 'degrees')
+        cam_r, cam_theta, cam_phi = spherical3d(rotated_cam_vector[0,0], rotated_cam_vector[1,0], rotated_cam_vector[2,0], angle_unit = 'degrees')
         # Calculate the impact of the led in question on the pixel in question (impact accounts for reflected light and attenuation of incident and reflected lights
         reflectivity_mu_theta = inc_theta + 180 # degrees. Incident light gets reflected primarily at incident azimuth+180 
         reflectivity_mu_phi = inc_phi # degrees. As far as polar angle is concerned, it doesn't change, as the angle of reflection = angle of incidence
-        reflection_factor = (1/(2*pi*reflectivity_sigma**2)) * np.exp(-0.5 * ((cam_theta - reflectivity_mu_theta)**2+(cam_phi - reflectivity_mu_phi)**2) / reflectivity_sigma**2) 
-        inc_attenuation = 1/(la.norm(led_plane[:,led] - pixel[:,pixel])**2) 
-        cam_attenuation = 1/(la.norm(cam_location - pixel[:,pixel])**2)
+        reflection_factor = (1/(2*np.pi*reflectivity_sigma**2)) * np.exp(-0.5 * ((cam_theta - reflectivity_mu_theta)**2+(cam_phi - reflectivity_mu_phi)**2) / reflectivity_sigma**2) 
+        inc_attenuation = 1/(la.norm(led_plane[:,led] - pixels[:,pixel])**2) 
+        cam_attenuation = 1/(la.norm(cam_location - pixels[:,pixel])**2)
         pix_led_impact_mat[pixel, led] = cam_attenuation * reflection_factor * inc_attenuation
 
-led_brightness = np.zeros([num_leds, 1]) # lux value of LEDs in the mat
+led_brightness = np.ones([num_leds, 1])*led_max_output # lux value of LEDs in the mat
 pix_brightness = pix_led_impact_mat @ led_brightness 
 
 ## Visualization
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d') 
-led_wf = ax.plot_wireframe(X = led_x, Y = led_y, Z = led_z, color = 'black')
-sphere_wf = ax.plot_wireframe(X = pix_x, Y = pix_y, Z = pix_z, color = 'blue')
+fig1 = plt.figure(1)
+ax1 = fig1.add_subplot(111, projection='3d') 
+led_wf = ax1.plot_wireframe(X = led_x, Y = led_y, Z = led_z, color = 'black')
+sphere_wf = ax1.plot_wireframe(X = pix_x, Y = pix_y, Z = pix_z, color = 'blue')
 
 # Customize the z axis.
-ax.set_xlim(-3, 3)
-ax.set_ylim(-3, 3)
-ax.set_zlim(-3, 3)
+ax1.set_xlim(-3, 3)
+ax1.set_ylim(-3, 3)
+ax1.set_zlim(-3, 3)
 #ax.zaxis.set_major_locator(LinearLocator(10))
 #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
 # fig.colorbar(surf, shrink=0.5, aspect=5)
-ax.set_xlabel('X axis')
-ax.set_ylabel('Y axis')
-ax.set_zlabel('Z axis')
+ax1.set_xlabel('X axis')
+ax1.set_ylabel('Y axis')
+ax1.set_zlabel('Z axis')
+
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(111)
+Xn = np.arange(0,num_pixels,1)
+ax2.plot(Xn, pix_brightness.flatten())
+
 plt.show()
